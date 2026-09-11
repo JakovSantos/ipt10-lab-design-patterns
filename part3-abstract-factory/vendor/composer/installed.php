7bcc35d9afffc3d910141f47311da32277d73c2d
Jakov Santos <santos.jakovbenedict@student.auf.edu.ph>
2026-09-11 18:04:12 +0800

    Part 3: Add Berserker class to Abstract Factory demo

---
+++ part3-abstract-factory/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'cb755ce63d2dcc89dca0494cf2bc091273eccfc4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cb755ce63d2dcc89dca0494cf2bc091273eccfc4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
